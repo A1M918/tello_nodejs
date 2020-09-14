ed496ab5bb91f32928704d3654d3f18da77142ba
Aamir Mughal <9.one.eight@gmail.com>
2020-09-14 23:26:24 +0500

    key event added

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ process.env.PORT = 8889
 process.env.HOST = '192.168.10.1'
 
 const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
+readline.emitKeypressEvents(process.stdin);
+
+process.stdin.setRawMode(true);
+
+process.stdin.on('keypress', function (chunk) {
+  if(chunk.toString() === '\`') process.exit(0);
+  console.log(`====> ${chunk}`);
+});
 const Tello = require('./tello/Tello');
 
 console.log('Hello World');
 
-const tello = new Tello()
+const tello = new Tello();
 
 async function main(){
   let action = true;
